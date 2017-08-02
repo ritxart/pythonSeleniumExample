242c9c277215d53ec5143df6b1e75dd6d9d306e0
ritxart <roc.itxart1@gmail.com>
2017-08-02 15:21:22 +0200

    added unitetest

--- test.py
+++ test.py
@@ -6,40 +6,44 @@ from selenium.webdriver.common.by import By
 import random
 import string
 import HomePage
+import unittest
 
 ##### TO REMOVE
 
-class SingInTests():
-#	def setUp(self):
-		driver = webdriver.Chrome('/Library/Python/2.7/site-packages/selenium/webdriver/chrome/chromedriver')
-		driver.get("https://www.goduo.com/")
+class SingInTests(unittest.TestCase):
+	def setUp(self):
+		self.driver = webdriver.Chrome('/Library/Python/2.7/site-packages/selenium/webdriver/chrome/chromedriver')
+		self.driver.get("https://www.goduo.com/")
 
-#	def test_create_new_user(self):
+	def test_create_new_user(self):
 		home_page = HomePage.HomePageObject()
 		birth_date = [random.randint(1,31),random.randint(1,12),random.randint(1937,1999)]
 		
 		sex = [random.randint(0,1), random.randint(0,1)]
-		iam = driver.find_element(By.XPATH, "//label[@for='" + home_page.iam[sex[0]] + "']").click()
-		looking = driver.find_element(By.XPATH, "//label[@for='" + home_page.looking[sex[1]] + "']").click()
+		iam = self.driver.find_element(By.XPATH, "//label[@for='" + home_page.iam[sex[0]] + "']").click()
+		looking = self.driver.find_element(By.XPATH, "//label[@for='" + home_page.looking[sex[1]] + "']").click()
 
-		day = Select(driver.find_element_by_name(home_page.birthdayDay))
+		day = Select(self.driver.find_element_by_name(home_page.birthdayDay))
 		day.select_by_value(str(birth_date[0]))
 		
-		month = Select(driver.find_element_by_name(home_page.birthdayMonth))
+		month = Select(self.driver.find_element_by_name(home_page.birthdayMonth))
 		month.select_by_value(str(birth_date[1]).zfill(2))
 		
-		year = Select(driver.find_element_by_name(home_page.birthdayYear))
+		year = Select(self.driver.find_element_by_name(home_page.birthdayYear))
 		year.select_by_value(str(birth_date[2]))
 
-		region = Select(driver.find_element_by_id(home_page.region))
+		region = Select(self.driver.find_element_by_id(home_page.region))
 		region.select_by_value(str(random.randint(1,50)).zfill(2))
 
 		## MODIFYYYYY !!!!
-		email = driver.find_element_by_name(home_page.email).send_keys("roc.itxart@redbooth.com")
+		email = self.driver.find_element_by_name(home_page.email).send_keys("roc.itxart@redbooth.com")
 		
-		user = driver.find_element_by_name(home_page.user).send_keys(''.join(random.sample(string.ascii_lowercase, 10)))
+		user = self.driver.find_element_by_name(home_page.user).send_keys(''.join(random.sample(string.ascii_lowercase, 10)))
 
-		password = driver.find_element_by_name(home_page.password).send_keys(''.join(random.sample(string.ascii_lowercase, 10)))
-		driver.find_element_by_name("register").click()
-#	def tearDown(self):
-		driver.quit()
+		password = self.driver.find_element_by_name(home_page.password).send_keys(''.join(random.sample(string.ascii_lowercase, 10)))
+		self.driver.find_element_by_name("register").click()
+	def tearDown(self):
+		self.driver.quit()
+
+if __name__=="__main__":
+    unittest.main()
